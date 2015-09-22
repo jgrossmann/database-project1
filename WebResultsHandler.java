a9a02873604cbd8c420a268dc41f08ce102e5f85
John Grossmann <jgrossmann@colgate.edu>
2015-09-22 15:38:55 -0400

    update

--- WebResultsHandler.java
+++ WebResultsHandler.java
@@ -2,14 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class WebResultsHandler {
-	List<WebResult> results;
 	double precision;
 	PrintWriter transcript;
+	
 
 	public WebResultsHandler(String results, double precision, PrintWriter transcript) {
 		this.precision = precision;
 		this.transcript = transcript;
 		parseResults(results);
+	}
+
+
+	public void println(String text) {
+		System.out.println(text);
+		transcript.println(text);
+	}
+
+	
+	public void print(String text) {
+		System.out.print(text);
+		transcript.print(text);
 	}
 
 
@@ -20,6 +32,33 @@ public class WebResultsHandler {
 	public boolean relevanceFeedback() {
 		//iterate over all results and record each relevance based on user feedback
 		//return true if precision is met, false otherwise
+		int numRelevant = 0;
+		Scanner in = new Scanner(System.in);
+		int i = 0;
+		for(WebResult result : results) {
+			println("Result "+i);
+			println("[");
+			println(" URL:  "+result.url);
+			println(" Title:  "+result.title);
+			println(" Summary:  "+result.summary);
+			println("]\n");
+			
+			while(true) {
+				print("Relevant (Y/N)?");
+				String feedback = in.nextLine()
+				transcript.println(feedback);
+				if(feedback.equalsIgnoreCase("N")) {
+					result.isRelevant(false);
+				}else if(feedback.equalsIgnoreCase("Y")) {
+					result.isRelevant(true);
+					numRelevant++;
+				}else {
+					println("Please enter only 'Y' or 'N'");
+				}	
+			}
+			i++;
+		}
+		
 		return true;
 	}
 
