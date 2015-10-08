0022608314c2c4e682bddfa64f61abbca341fa86
John Grossmann <jgrossmann@colgate.edu>
2015-10-08 16:04:28 -0400

    update

---
+++ src/WordWeight.java
@@ -0,0 +1,10 @@
+//holds a word with its weight in the returned results
+class WordWeight {
+    Double weight;
+    String word;
+    
+    public WordWeight(String word, Double weight) {
+      this.weight = weight;
+      this.word = word;
+    }
+}
---
+++ src/WordWeightComparator.java
@@ -0,0 +1,24 @@
+
+//comparator for wordweights for use with priority queue
+class WordWeightComparator implements Comparator<WordWeight> {
+    public int compare(WordWeight a, WordWeight b) {
+        if(a.weight > b.weight) {
+            return -1;
+        }else if(a.weight < b.weight) {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    public boolean equals(Object a, Object b) {
+        WordWeight A = (WordWeight)a;
+        WordWeight B = (WordWeight)b;
+        if(A.word.equalsIgnoreCase(B.word)) {
+            if(A.weight == B.weight) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
